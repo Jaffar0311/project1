5d642a1be9d6440d18ff1f96648d0978a32ab2eb
amsvang <amyvangsinbox@outlook.com>
2022-02-04 10:01:42 -0800

    made appExceptionHandler and updated JavalinApp

--- src/main/java/com/revature/JavalinApp.java
+++ src/main/java/com/revature/JavalinApp.java
@@ -1,4 +1,55 @@
 package com.revature;
 
+import com.revature.controllers.AppExceptionHandler;
+import com.revature.controllers.AuthController;
+import com.revature.controllers.UserController;
+import com.revature.util.LoggingUtil;
+import io.javalin.Javalin;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
 public class JavalinApp {
+
+    private final LoggingUtil loggingUtil = new LoggingUtil();
+    private final AppExceptionHandler appExceptionHandler = new AppExceptionHandler();
+    private AuthController authController = new AuthController();
+    private final UserController userController = new UserController();
+
+    private Javalin app = Javalin.create().routes(()->{
+
+        //TODO: UPDATE ADMIN ACCESS ROUTES
+        /*path("admin-access",()->{
+            before(authController::authorizeManageToken);
+            path("account",()->{
+                get(accountController::handleGetAllAccounts);
+                post(accountController::handleCreate);
+                delete(accountController::handleDelete);
+                path("{id}",()->{
+                    get(accountController::handleGetOne);
+                    put(accountController::handleUpdate);
+                });
+            });
+        });*/
+
+        path("employee-access",()->{
+            before(authController::authorizeEmployeeToken);
+            path("user",()->{
+                path("{id}",()->{
+                    get(userController::handleGetOne);
+                    put(userController::handleUpdate);
+                });
+            });
+        });
+
+        path("login", ()->{
+            post(authController::authenticateLogin);
+        });
+        before("*",loggingUtil::logRequest);
+    }).exception(NumberFormatException.class, appExceptionHandler::handleNumberFormatException);
+
+    public void start(int port){
+        app.start(port);
+        }
+
+
 }
