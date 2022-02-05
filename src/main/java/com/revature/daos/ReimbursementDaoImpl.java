3c2c9e207560d37a789453dbb448471df37acffe
ajaffar0311 <67613243+ajaffar0311@users.noreply.github.com>
2022-02-05 14:20:09 -0600

    adding new reimbursement

--- src/main/java/com/revature/daos/ReimbursementDaoImpl.java
+++ src/main/java/com/revature/daos/ReimbursementDaoImpl.java
@@ -11,13 +11,13 @@ import java.sql.SQLException;
 public class ReimbursementDaoImpl implements ReimbursementDAO {
     @Override
     public boolean addReimbursement(Reimbursement reimbursement) {
-        String sql = "insert into ers_reimbursement (users_id,reimb_type,status_type,reimb_amount,reimb_submitted,reimb_resolved, reimb_description,reimb_receipt) values(?,?,?, ?, ?,? ,?,?)";
+        String sql = "insert into ers_reimbursement (users_id,reimb_type,status_type,reimb_amount,reimb_submitted,reimb_resolved, reimb_description,reimb_receipt) values(?,CAST(? AS ERS_REIMBURSEMENT_TYPE),CAST(? AS ERS_REIMBURSEMENT_STATUS), ?, ?,? ,?,?)";
         try (Connection conn = ConnectionUtil.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);){
 
             ps.setInt(1, reimbursement.getUser_id());
-            ps.setString(2, reimbursement.getReimbursementType().name()); //org.postgresql.util.PSQLException: ERROR: column "reimb_type" is of type ers_reimbursement_type but expression is of type character varying
-            ps.setInt(3, reimbursement.getReimbursementStatus().ordinal());
+            ps.setString(2, reimbursement.getReimbursementType().name());
+            ps.setString(3, reimbursement.getReimbursementStatus().name());
             ps.setDouble(4, reimbursement.getReimbursementAmount());
             ps.setBoolean(5, reimbursement.isReimbusementSubmitted());
             ps.setBoolean(6, reimbursement.isReimbusementResolved());
